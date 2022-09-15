1d85af96621be73210afc65c843289413aaf7279
TovStol <104901166+TovStol@users.noreply.github.com>
2022-09-15 20:23:53 +0300

    all

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,90 +1,38 @@
 package jm.task.core.jdbc.service;
 
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
-import jm.task.core.jdbc.util.Util;
 
 
-import java.sql.*;
-import java.util.ArrayList;
 import java.util.List;
 
 
 public class UserServiceImpl implements UserService {
-    private Util util = new Util();
+    UserDaoJDBCImpl userDaoJDBC = new UserDaoJDBCImpl();
 
     public void createUsersTable() {
-        String sqlCommand = "CREATE TABLE IF NOT EXISTS users " + "(id BIGINT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(20) NOT NULL , " + "lastName VARCHAR(20) NOT NULL , age SMALLINT NOT NULL)";
-        try (Connection connection = util.getConnection(); Statement statement = connection.createStatement()) {
-
-            statement.executeUpdate(sqlCommand);
-        } catch (SQLException e) {
-            System.out.println("Таблица не создана");
-        }
+        userDaoJDBC.createUsersTable();
     }
 
     public void dropUsersTable() {
-        String sqlCommand = "DROP TABLE IF EXISTS users";
-
-        try (Connection connection = util.getConnection(); Statement statement = connection.createStatement()) {
-            statement.executeUpdate(sqlCommand);
-        } catch (SQLException e) {
-            System.out.println("Таблица не удалена");
-        }
+        userDaoJDBC.dropUsersTable();
     }
 
     public void saveUser(String name, String lastName, byte age) {
+        userDaoJDBC.saveUser(name, lastName, age);
 
-        String sqlCommand = "INSERT INTO store.users (name, lastName, age ) VALUES (?, ?, ?)";
-
-        try (Connection connection = util.getConnection();
-             PreparedStatement preparedStatement = connection.prepareStatement(sqlCommand)) {
-            preparedStatement.setString(1, name);
-            preparedStatement.setString(2, lastName);
-            preparedStatement.setByte(3, age);
-            preparedStatement.executeUpdate();
-            System.out.printf("User с именем – %s добавлен в базу данных\n", name);
-        } catch (SQLException e) {
-            System.out.println("User не добавлен");
-
-        }
     }
 
     public void removeUserById(long id) {
-        String sqlCommand = "DELETE FROM store.users WHERE id=?";
-        try (Connection connection = util.getConnection(); PreparedStatement preparedStatement = connection.prepareStatement(sqlCommand)) {
-            preparedStatement.setLong(1, id);
-            preparedStatement.executeUpdate();
-        } catch (SQLException e) {
-            System.out.println("User не удален");
-        }
+        userDaoJDBC.removeUserById(id);
     }
 
     public List<User> getAllUsers() {
-        List<User> userList = new ArrayList<>();
-        String sqlCommand = "SELECT * FROM store.users";
-        try (Connection connection = util.getConnection(); Statement statement = connection.createStatement()) {
-            ResultSet resultSet = statement.executeQuery(sqlCommand);
-            while (resultSet.next()) {
-                User user = new User();
-                user.setId(resultSet.getLong("id"));
-                user.setName(resultSet.getString("name"));
-                user.setLastName(resultSet.getString("lastname"));
-                user.setAge(resultSet.getByte("age"));
-
-                userList.add(user);
-            }
-        } catch (SQLException e) {
-            System.out.println("Ошибка получения всех User");
-        }
-        return userList;
+        return userDaoJDBC.getAllUsers();
     }
 
     public void cleanUsersTable() {
-        String sqlCommand = "DELETE FROM store.users";
-        try (Connection connection = util.getConnection(); Statement statement = connection.createStatement()) {
-            statement.executeUpdate(sqlCommand);
-        } catch (SQLException e) {
-            System.out.println("User не удален");
-        }
+        userDaoJDBC.cleanUsersTable();
     }
+
 }
